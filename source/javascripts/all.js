abaf1895a220c5922669341506c981c8edc2f358
Shin'ya Ueoka <ueokande@i-beam.org>
2016-10-04 08:00:21 +0900

    Add a link to email

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,6 @@
-// This is where it all goes :)
+'use strict'
+
+window.onload = function() {
+  var emailElement = document.querySelector('.service-icons a.email')
+  emailElement.href = String.fromCharCode(109,97,105,108,116,111,58,117,101,111,107,97,110,100,101,64,105,45,98,101,97,109,46,111,114,103)
+}
